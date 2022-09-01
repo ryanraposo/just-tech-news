1f0383a9cb663391415681cc18108de2d537b222
Ryan Raposo <raposo.ryan@gmail.com>
2022-09-01 17:10:07 -0400

    add login route and logic

--- models/User.js
+++ models/User.js
@@ -4,7 +4,11 @@ const sequelize = require('../config/connection');
 
 
 
-class User extends Model {}
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 
 User.init(
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -42,12 +42,36 @@ router.post('/', (req, res) => {
         username: req.body.username,
         email: req.body.email,
         password: req.body.password
-    })
-        .then(dbUserData => res.json(dbUserData))
+    }).then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+// POST /api/login
+router.post('/login', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address' });
+            return;
+        }
+
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    })
+
 });
 
 // PUT /api/users/1
