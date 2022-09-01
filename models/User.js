93cd68bd94fb800a93addd3af6a3904b63a74c0f
Ryan Raposo <raposo.ryan@gmail.com>
2022-09-01 15:18:10 -0400

    add user model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class User extends Model {}
+
+
+User.init(
+    {
+        // TABLE COLUMN DEFINITIONS
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        // TABLE CONFIG OPTIONS GO HERE
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+
+module.exports = User;
