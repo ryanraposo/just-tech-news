ca8724aea3b82573e6afa67b87a550b4cf4c7b43
Ryan Raposo <raposo.ryan@gmail.com>
2022-09-02 13:00:54 -0400

    add post routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
 
 module.exports = { User, Post };
