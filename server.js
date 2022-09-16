6cf3fe266d684b641e558d4480ccdc0f708890ff
Ryan Raposo <raposo.ryan@gmail.com>
2022-09-16 13:44:01 -0400

    optimize and cleanup

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,7 +23,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
